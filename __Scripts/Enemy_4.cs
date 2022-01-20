7361455c46dd69c8b4e0b8016986344ee92fc61b
Margareti95 <56870474+Margareti95@users.noreply.github.com>
2022-01-20 15:01:06 +0300

    Скрипты проекта

---
+++ __Scripts/Enemy_4.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_4 : Enemy
+{
+    private Vector3 p0, p1;
+    private float timeStart;
+    private float duration = 4;
+
+    private void Start() {
+
+        p0 = p1 = pos;
+        InitMovement();
+    }
+
+    void InitMovement()
+    {
+        p0 = p1;
+        float winMinRad = bndCheck.camWidth - bndCheck.radius;
+        float hgtMinRad = bndCheck.camHeight - bndCheck.radius;
+
+        p1.x = Random.Range(-winMinRad, winMinRad);
+        p1.y = Random.Range(-hgtMinRad, hgtMinRad);
+
+        timeStart = Time.time;
+    }
+
+    public override void Move()
+    {
+        float u = (Time.time - timeStart)/duration;
+
+        if(u>=1)
+        {
+            InitMovement();
+            u=0;
+        }
+
+        u=1-Mathf.Pow(1-u, 2);
+        pos = (1-u)*p0 + u*p1;
+    }
+}
